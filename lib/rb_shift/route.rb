ced13083472fb7fe9f66050b68d602ab528e3ae1
Jakub Cechacek <jcechace@gmail.com>
2017-09-21 01:10:57 +0200

    Ability to extract URL from route

--- lib/rb_shift/route.rb
+++ lib/rb_shift/route.rb
@@ -4,6 +4,15 @@
 require_relative 'openshift_kind'
 
 module RbShift
+  # Representation of OpenShift route
   class Route < OpenshiftKind
+    # Constructs route address
+    # @return [String] address
+    def address
+      host        = obj[:spec][:host]
+      termination = obj[:spec][:termination]
+      protocol    = termination ? 'https' : 'http'
+      "#{protocol}://#{host.chomp '/'}"
+    end
   end
 end
